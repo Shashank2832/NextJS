c00889a370f10f9f2f557f9ca91a9456bd1d0cd9
Shashank2832 <shaggy2832@gmail.com>
2023-05-22 20:40:29 +0530

    Routing based nextJS project

---
+++ routing/pages/404.js
@@ -0,0 +1,11 @@
+function NotFoundPage() {
+    return(
+        <div>
+            <h1>
+                Page Not Found
+            </h1>
+        </div>
+    )
+}
+
+export default NotFoundPage;
---
+++ routing/pages/portfolio/[projectid].js
@@ -0,0 +1,16 @@
+import { useRouter } from "next/router";
+
+function PortfolioProjectPage () {
+
+    const router = useRouter();
+
+    return(
+        <div>
+            <h1>
+                Portfolio Project Page
+            </h1>
+        </div>
+    )
+}
+
+export default PortfolioProjectPage;
